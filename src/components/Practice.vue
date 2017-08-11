284f51e85230bd85f0ee254f55d1b3c497065244
cheolwoong <chulwung7@gmail.com>
2017-08-11 15:19:02 +0900

    Component Page

---
+++ src/components/Practice.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="Practice">
+  뿌렉티스요
+</div>
+</template>
+
+<script>
+export default {
+  name: 'practice',
+//  methods: {
+//    dateClick() {
+//      this.$emit('p-input-click');
+//    },
+//  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
