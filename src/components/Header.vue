e6d59472c3574061e14de522e4b6318b84ae4b16
cheolwoong <chulwung7@gmail.com>
2017-08-11 18:38:13 +0900

    make header component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="GNB">
+    <img @click="logoClick" src="../assets/images/logo_org.png" >
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    methods: {
+      logoClick() {
+        this.$emit('transLogoClick');
+      },
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #GNB {
+    width: 100%;
+    height: 80px;
+    text-align: center;
+  }
+  #GNB > img {
+    margin: 5px;
+    height: 70px;
+    cursor: pointer;
+  }
+</style>
