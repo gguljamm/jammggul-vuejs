449ca74ffb8af3ed882ae5f2df069bc8fc57d3e5
matthew_lee <matthew_lee@gccompany.co.kr>
2022-02-21 15:18:37 +0900

    for IntersectionObserve!

---
+++ src/components/Portfolio/PortfolioItem.vue
@@ -0,0 +1,253 @@
+<template>
+  <li class="portfolioItem" ref="item" v-bind:class="isMobile?'mob':''">
+    <div>
+      <div class="contImg"><div :style="{
+          backgroundImage: `url(${item.thumbnail?item.thumbnail:'/static/thumbnail.jpg'})`
+        }"></div></div>
+      <div class="contTitle" v-html="item.title"></div>
+      <div class="contSpec"><span v-for="spec in item.spec">{{ spec }}</span></div>
+      <div class="contDate" v-if="item.date">[{{ item.date }}]</div>
+      <ul v-if="item.participate" class="contPart">
+        <li v-for="part in item.participate">
+          <div class="partTitle">{{ part.title }}</div>
+          <div class="partRate"><div v-bind:style="{width: part.width}">{{ part.rate }}%</div></div>
+        </li>
+      </ul>
+      <div class="hover">
+        <div>
+          <button v-for="button in item.url"@click="urlClick(button.clickEvent)"><span>{{ button.name }}</span></button>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['item', 'isMobile'],
+  data() {
+    return {
+      observer: null,
+    };
+  },
+  methods: {
+    urlClick(url) {
+      if (typeof url === 'function') {
+        url();
+      } else {
+        window.open(url, '_blank');
+      }
+    },
+  },
+  mounted() {
+    this.observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && this.item.participate) {
+        for (let y = 0; y < this.item.participate.length; y += 1) {
+          this.item.participate[y].width = `${this.item.participate[y].rate}%`;
+        }
+      }
+    }, {
+      root: null,
+      threshold: 0.5,
+    });
+    this.observer.observe(this.$refs.item);
+  },
+  beforeDestroy() {
+    this.observer.disconnect();
+  },
+};
+
+</script>
+
+<style scoped>
+.portfolioItem{
+  width: 50%;
+  float: left;
+  padding: 0 10px;
+  padding-bottom: 20px;
+}
+.portfolioItem:nth-child(odd){
+  clear: both;
+}
+.portfolioItem > div{
+  background-color: #FFF;
+  border: 1px solid #e1e3e5;
+  -webkit-box-shadow: 1px 1px 1px 0 rgba(0,0,0,.15);
+  -moz-box-shadow: 1px 1px 1px 0 rgba(0,0,0,.15);
+  box-shadow: 1px 1px 1px 0 rgba(0,0,0,.15);
+  position: relative;
+}
+.portfolioItem .contImg{
+  padding: 5px;
+}
+.portfolioItem .contImg > div{
+  width: 100%;
+  padding-bottom: 60%;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.portfolioItem .contTitle{
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 30px;
+  padding: 10px 0;
+  text-align: center;
+}
+.portfolioItem .contSpec{
+  text-align: center;
+  padding-bottom: 10px;
+  line-height: 32px;
+}
+.portfolioItem .contDate{
+  text-align: center;
+  padding-bottom: 10px;
+  line-height: 32px;
+}
+.portfolioItem .contSpec > span{
+  background-color: skyblue;
+  display: inline-block;
+  height: 30px;
+  line-height: 30px;
+  -webkit-border-radius: 15px;
+  -moz-border-radius: 15px;
+  border-radius: 15px;
+  margin-right: 5px;
+  padding: 0 10px;
+  color: #FFF;
+  font-size: 14px;
+}
+.portfolioItem .contSpec > span:last-child{
+  margin-right: 0;
+}
+.portfolioItem .contPart{
+  padding: 20px;
+  width: 90%;
+  margin: 0 auto;
+  border: 1px solid #a7d2cb;
+  margin-bottom: 30px;
+}
+.portfolioItem .contPart > li{
+  height: 30px;
+  line-height: 30px;
+  margin-bottom: 10px;
+  font-size: 14px;
+}
+.portfolioItem .contPart > li:last-child{
+  margin-bottom: 0;
+}
+.portfolioItem .contPart > li .partTitle{
+  width: 30%;
+  float: left;
+  text-align: center;
+  padding-right: 10px;
+  white-space: nowrap;
+}
+.portfolioItem .contPart > li .partRate{
+  width: 70%;
+  float: left;
+  -webkit-border-radius: 15px;
+  -moz-border-radius: 15px;
+  border-radius: 15px;
+}
+.portfolioItem .contPart > li .partRate > div{
+  text-align: center;
+  line-height: 30px;
+  color: #FFF;
+  -webkit-border-radius: 15px;
+  -moz-border-radius: 15px;
+  border-radius: 15px;
+  width: 0;
+  overflow: hidden;
+  transition: width 1s ease;
+}
+.portfolioItem .contPart > li:nth-child(1) .partRate > div{
+  background-color: #ffb3ba;
+}
+.portfolioItem .contPart > li:nth-child(3) .partRate > div{
+  background-color: #ffdfba;
+}
+.portfolioItem .contPart > li:nth-child(5) .partRate > div{
+  background-color: #ffffba;
+}
+.portfolioItem .contPart > li:nth-child(2) .partRate > div{
+  background-color: #baffc9;
+}
+.portfolioItem .contPart > li:nth-child(4) .partRate > div{
+  background-color: #bae1ff;
+}
+.portfolioItem .hover{
+  opacity: 0;
+  background-color: rgba(0,0,0,.7);
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  transition: opacity .2s ease;
+}
+.portfolioItem > div:hover .hover{
+  opacity: 1;
+}
+.portfolioItem.mob .hover{
+  opacity: 1;
+  background-color: transparent;
+  position: relative;
+  margin-bottom: 10px;
+}
+.portfolioItem > div .hover > div{
+  position: relative;
+  top: 50%;
+  transform: translate(0, -50%);
+  text-align: center;
+}
+.portfolioItem.mob > div .hover > div{
+  position: relative;
+  width: 100%;
+  top: 0;
+  right: 0;
+  transform: none;
+}
+.portfolioItem > div .hover button{
+  cursor: pointer;
+  margin-right: 16px;
+  height: 40px;
+  line-height: 40px;
+  color: #FFF;
+  border: 0;
+  padding: 0 10px;
+  -webkit-box-shadow: 1px 1px 1px 0 rgba(0,0,0,.2);
+  -moz-box-shadow: 1px 1px 1px 0 rgba(0,0,0,.2);
+  box-shadow: 1px 1px 1px 0 rgba(0,0,0,.2);
+  position: relative;
+}
+.portfolioItem > div .hover button > span{
+  z-index: 2;
+  position: relative;
+}
+.portfolioItem > div .hover button:hover:after{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index:1;
+  background-color: rgba(0,0,0,.2);
+}
+.portfolioItem > div .hover button:nth-child(1){
+  background-color: #f2d388;
+}
+.portfolioItem > div .hover button:nth-child(2){
+  background-color: #a7d2cb;
+}
+.portfolioItem > div .hover button:last-child{
+  margin-right: 0;
+}
+@media all and (max-width: 768px){
+  .portfolioItem{
+    float: none;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/Portfolio/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div id="Practice">
+    <div class="portfolioHead">
+      <button @click="authClick()">
+        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+        <span>Write</span>
+      </button>
+    </div>
+    <input-portfolio
+      v-if="isAuth"
+      @close-pop="closePop"
+      @reload="getData"
+    ></input-portfolio>
+    <ul class="ulPrac">
+      <portfolio-item v-for="temp in list" :item="temp" :isMobile="isMobile" />
+    </ul>
+    <div class="popPrac"></div>
+  </div>
+</template>
+
+<script>
+import inputPortfolio from '../Input/Input-Portfolio';
+import PortfolioItem from './PortfolioItem';
+
+export default {
+  components: {
+    inputPortfolio,
+    PortfolioItem,
+  },
+  name: 'practice',
+  props: ['isMobile'],
+  methods: {
+    async authClick() {
+      if (this.isAuth) {
+        this.isAuth = false;
+        return;
+      }
+      this.isAuth = await this.$firebase.login();
+    },
+    closePop() {
+      const temp = confirm('정말 그만쓸거야?');
+      if (temp) {
+        this.isAuth = false;
+      }
+    },
+    getData() {
+      this.list = [];
+      this.isAuth = false;
+      this.$firebase.database('/portfolio').once('value', (snap) => {
+        const list = snap.val();
+        const keys = Object.values(list);
+        keys.reverse().forEach((x) => {
+          const content = {
+            title: x.title,
+            thumbnail: x.thumbnail,
+            spec: x.spec,
+            date: x.date,
+            url: x.operate ? x.operate.map(v => ({
+              name: v.name,
+              clickEvent: v.url,
+            })) : [],
+            participate: x.contribute ? x.contribute.map(v => ({
+              title: v.name,
+              width: 0,
+              rate: v.percent,
+            })) : [],
+          };
+          this.list.push(content);
+        });
+      });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+  data() {
+    return {
+      isAuth: false,
+      list: [],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.portfolioHead{
+  height: 55px;
+  padding: 10px;
+}
+.portfolioHead > button{
+  height: 35px;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  padding: 0 20px;
+  cursor: pointer;
+  font-size: 14px;
+  border: 0;
+  color: #FFF;
+  background-color: #c98474;
+  left: 10px;
+  float: left;
+}
+.portfolioHead i{
+  margin-right: 5px;
+}
+#Practice{
+  max-width: 1080px;
+  width: 100%;
+  margin: 0 auto;
+  font-family: "Noto Sans KR", sans-serif;
+  padding-bottom: 20px;
+}
+.ulPrac{
+  overflow: auto;
+  margin: 0;
+}
+</style>
